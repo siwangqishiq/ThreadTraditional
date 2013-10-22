e6b4a4adb11979887e912f4f220d9d14d9be7b45
siwangqishiq <525647740@qq.com>
2013-10-22 15:23:10 +0800

    线程同步问题

--- ThreadTraditional/src/com/xinlan/traditional/TraditionalThreadCommunication.java
+++ ThreadTraditional/src/com/xinlan/traditional/TraditionalThreadCommunication.java
@@ -5,11 +5,66 @@ public class TraditionalThreadCommunication {
 	/**
 	 * 线程间的同步通信
 	 * 
-	 * 子线程循环10次 接着主线程循环100次 接着又回到子线程循环10次
-	 * 接着再回到主线程又循环100次        如此循环50次     写出程序
-	 * @param 
+	 * 子线程循环10次 接着主线程循环100次 接着又回到子线程循环10次 接着再回到主线程又循环100次 如此循环50次 写出程序
+	 * 
+	 * @param
 	 */
 	public static void main(String[] args) {
-		
+		new TraditionalThreadCommunication().init();
 	}
-}//end class
+
+	private void init(){
+		final Business business = new Business();
+		
+		new Thread(new Runnable(){
+			@Override
+			public void run() {
+				for(int i=1;i<=50;i++){
+					business.subDo(i);
+				}//end for i
+			}
+		}).start();
+		
+		for(int i=1;i<=50;i++){
+			business.mainDo(i);
+		}//end for i
+	}
+	
+	private final class Business {
+		private boolean shouldSubRun=true;
+		
+		public synchronized void subDo(int count) {
+			while(!shouldSubRun){
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}//end while
+			
+			for (int i = 1; i <= 10; i++) {
+				System.out.println("sub-->"+i+",   count--->"+count);
+			}// end for
+			
+			shouldSubRun=false;
+			this.notify();
+		}
+
+		public synchronized void mainDo(int count) {
+			while(shouldSubRun){
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}//end while
+			
+			for (int i = 1; i <= 20; i++) {
+				System.out.println("main-->"+i+",   count--->"+count);
+			}// end for
+			
+			shouldSubRun = true;
+			this.notify();
+		}
+	}// end inner class
+}// end class
